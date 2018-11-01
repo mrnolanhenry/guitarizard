b4ae586f8d99295776ffc82215b2763f1fa96ad0
Fresh Banana Smile <enfenry@vt.edu>
2018-11-01 11:40:04 -0400

    scales verified and updated

--- packages/note-lib-good/src/data/scales.js
+++ packages/note-lib-good/src/data/scales.js
@@ -8,6 +8,7 @@ module.exports = [
   // algerian scale is super funky changes based on octave -- more accurately [0,2,3,6,7,8,11,12,14,15,17,19,20,23,24]
 
   new Scale('altered', diatonic, [0,1,3,4,6,8,10,12]),
+  // altered scale AKA super Locrian AKA Locrian b4
 
   // new Scale('arabian (a)', diatonic, [0,2,3,5,6,8,9,11,12]),
   // arabian (a) wasn't found
@@ -64,10 +65,11 @@ module.exports = [
   // new Scale('diminished sevenths', diatonic, [0,3,6,9,12]),
   // diminished sevenths scale not found
 
-  new Scale('diminished half/whole', diatonic, [0,1,3,4,6,7,9,10,12]),
-  // diminished half/whole AKA diminished half
+  new Scale('diminished (half/whole)', diatonic, [0,1,3,4,6,7,9,10,12]),
+  // diminished half/whole AKA diminished half AKA octatonic (half/whole)
 
-  new Scale('diminished, whole/half', diatonic, [0,2,3,5,6,8,9,11,12]),
+  new Scale('diminished (whole/half)', diatonic, [0,2,3,5,6,8,9,11,12]),
+  // diminished whole/half AKA octatonic (whole/half)
 
   new Scale('dominant pentatonic', diatonic, [0,2,4,7,10,12]),
 
@@ -76,37 +78,70 @@ module.exports = [
   new Scale('double harmonic', diatonic, [0,1,4,5,7,8,11,12]),
   // double Harmonic AKA byzantine
 
-  // STOPPED BEFORE egyptian
   new Scale('egyptian', diatonic, [0,2,5,7,10,12]),
-  new Scale('eight tone spanish', diatonic, [0,1,3,4,5,6,8,10,12]),
+  new Scale('8 tone spanish', diatonic, [0,1,3,4,5,6,8,10,12]),
   new Scale('enigmatic', diatonic, [0,1,4,6,8,10,11,12]),
-  new Scale('ethiopian (a raray)', diatonic, [0,2,4,5,7,9,11,12]),
-  new Scale('ethiopian (geez & ezel)', diatonic, [0,2,3,5,7,8,10,12]),
-  new Scale('half diminished #2 (locrian #2)', diatonic, [0,2,3,5,6,8,10,12]),
-  new Scale('half diminished (locrian)', diatonic, [0,1,3,5,6,8,10,12]),
+
+  // new Scale('ethiopian (a raray)', diatonic, [0,2,4,5,7,9,11,12]),
+  // ethiopian (a raray) AKA ethiopian araray AKA major scale
+
+  new Scale('ethiopian (Geez)', diatonic, [0,2,3,5,7,8,10,12]),
+  // ethiopian Geez AKA ethiopian Ezel
+
+  new Scale('half diminished', diatonic, [0,2,3,5,6,8,10,12]),
+  // half diminished AKA locrian #2 AKA maybe half diminished #2
+
   new Scale('harmonic minor', diatonic, [0,2,3,5,7,8,11,12]),
+  // harmonic minor AKA mohammedan
+
   new Scale('hawaiian', diatonic, [0,2,3,5,7,9,11,12]),
+
   new Scale('hindu', diatonic, [0,2,4,5,7,8,10,12]),
-  new Scale('hirajoshi', diatonic, [0,2,3,7,8,12]),
-  new Scale('hirajoshi 2', diatonic, [0,4,5,9,11,12]),
+  // hindu scale AKA Aeolian Dominant AKA Mixolydian b6 (or b13)
+
+  new Scale('hirajōshi', diatonic,[0,4,5,9,11,12]),
+  // hirajōshi AKA hira-choshi
+  new Scale('hirajōshi 2', diatonic,[0,2,3,7,8,12]),
+  // hirajōshi AKA hira-choshi
+
   new Scale('hungarian gypsy', diatonic, [0,2,3,6,7,8,11,12]),
-  new Scale('hungarian gypsy persian', diatonic, [0,1,4,5,7,8,11,12]),
+  // Hungarian gypsy AKA Hungarian minor AKA double harmonic minor scale AKA Gypsy minor
+
+  // new Scale('hungarian gypsy persian', diatonic, [0,1,4,5,7,8,11,12]),
+  // hungarian gypsy persian scale not found
+
   new Scale('hungarian major', diatonic, [0,3,4,6,7,9,10,12]),
-  new Scale('hungarian minor', diatonic, [0,2,3,6,7,8,11,12]),
+
+  // new Scale('hungarian minor', diatonic, [0,2,3,6,7,8,11,12]),
+  // Hungarian gypsy AKA Hungarian minor AKA double harmonic minor scale AKA Gypsy minor
+
   new Scale('ionian', diatonic, [0,2,4,5,7,9,11,12]),
   new Scale('ionian #5', diatonic, [0,2,4,5,8,9,11,12]),
   new Scale('iwato', diatonic, [0,1,5,6,10,12]),
-  new Scale('japanese (a)', diatonic, [0,1,5,7,8,12]),
-  new Scale('japanese (b)', diatonic, [0,2,5,7,8,12]),
+
+  new Scale('kumoi', diatonic, [0,1,5,7,8,12]),
+  // kumoi AKA japanese (a) AKA maybe kumoi 2
+
+  new Scale('japanese 2', diatonic, [0,2,5,7,8,12]),
+  // japanese 2 AKA maybe japanese (b)
+
   new Scale('japanese (ichikosucho)', diatonic, [0,2,4,5,6,7,9,11,12]),
   new Scale('japanese (insen)', diatonic, [0,1,5,7,10,12]),
   new Scale('japanese (taishikicho)', diatonic, [0,2,4,5,6,7,9,10,11,12]),
   new Scale('javanese', diatonic, [0,1,3,5,7,9,10,12]),
   new Scale('jewish (adonai malakh)', diatonic, [0,1,2,3,5,7,9,10,12]),
-  new Scale('jewish (ahaba rabba)', diatonic, [0,1,4,5,7,8,10,12]),
-  new Scale('jewish (magen abot)', diatonic, [0,1,3,4,6,8,10,11,12]),
+
+  new Scale('phrygian dominant', diatonic, [0,1,4,5,7,8,10,12]),
+  // phrygian dominant AKA phrygian major AKA jewish (ahava rabba) AKA Freygish AKA altered Phrygian AKA dominant b2 b6 AKA Spanish Phrygian AKA Spanish Gypsy AKA Hijaz-Nahawand AKA Bayati maqam AKA Dastgāh-e Homāyoun
+
+  // new Scale('jewish (magen abot)', diatonic, [0,1,3,4,6,8,10,11,12]),
+  // jewish magen avot not found consistently
+
   new Scale('kumoi', diatonic, [0,2,3,7,9,12]),
+
   new Scale('kumoi 2', diatonic, [0,1,5,7,8,12]),
+  // japanese (a) AKA maybe kumoi 2
+
   new Scale('leading whole tone', diatonic, [0,2,4,6,8,10,11,12]),
   new Scale('locrian', diatonic, [0,1,3,5,6,8,10,12]),
   new Scale('locrian 6', diatonic, [0,1,3,5,6,9,10,12]),
@@ -114,29 +149,59 @@ module.exports = [
   new Scale('lydian #2', diatonic, [0,3,4,6,7,9,11,12]),
   new Scale('lydian augmented', diatonic, [0,2,4,6,8,9,11,12]),
   new Scale('lydian diminished', diatonic, [0,2,3,6,7,9,11,12]),
+  // lydian diminshed AKA lydian b3
+
   new Scale('lydian minor', diatonic, [0,2,4,6,7,8,10,12]),
+
   new Scale('major', diatonic, [0,2,4,5,7,9,11,12]),
+  // ethiopian (a raray) AKA ethiopian araray AKA major scale
+
   new Scale('major arpeggio', diatonic, [0,4,7,12]),
   new Scale('major blues', diatonic, [0,2,3,4,7,9,12]),
   new Scale('major locrian', diatonic, [0,2,4,5,6,8,10,12]),
   // major locrian AKA arabian (b)?
 
-  new Scale('melodic minor ascending', diatonic, [0,2,3,5,7,9,11,12]),
-  new Scale('melodic minor descending', diatonic, [0,2,3,5,7,8,10,12]),
-  new Scale('minor', diatonic, [0,2,3,5,7,8,10,12]),
+  new Scale('melodic minor', diatonic, [0,2,3,5,7,9,11,12]),
+  // melodic minor AKA melodic minor (ascending)
+
+  // new Scale('melodic minor (descending)', diatonic, [0,2,3,5,7,8,10,12]),
+  // melodic minor descending AKA natural minor AKA natural (pure) minor
+
   new Scale('minor arpeggio', diatonic, [0,3,7,12]),
-  new Scale('mixo-blues', diatonic, [0,3,4,5,6,7,10,12]),
+
+  new Scale('mixo-blues', diatonic, [0,2,3,4,5,6,7,9,10,12]),
+  // mixo-blues AKA mixolydian blues AKA  mixo-Dorian blues scale AKA mixolydian/blues hybrid
+
   new Scale('mixolydian', diatonic, [0,2,4,5,7,9,10,12]),
-  new Scale('mohammedan', diatonic, [0,2,3,5,7,8,11,12]),
+
+  // new Scale('mohammedan', diatonic, [0,2,3,5,7,8,11,12]),
+  // mohammedan AKA harmonic minor
+
   new Scale('moorish phrygian', diatonic, [0,1,3,4,5,7,8,10,11,12]),
-  new Scale('neopolitan', diatonic, [0,1,3,5,7,8,11,12]),
-  new Scale('neopolitan major', diatonic, [0,1,3,5,7,9,11,12]),
-  new Scale('neopolitan minor', diatonic, [0,1,3,5,7,8,10,12]),
+  // moorish phrygian AKA phrygian/double harmonic major mixed
+
+  new Scale('natural minor', diatonic, [0,2,3,5,7,8,10,12]),
+  // natural minor AKA melodic minor descending AKA natural (pure) minor
+
+  // new Scale('neapolitan', diatonic, [0,1,3,5,7,8,11,12]),
+  // neapolitan scale in this form not found - only the major and minor versions
+
+  new Scale('neapolitan major', diatonic, [0,1,3,5,7,9,11,12]),
+  new Scale('neapolitan minor', diatonic, [0,1,3,5,7,8,10,12]),
   new Scale('nine tone scale', diatonic, [0,2,3,4,6,7,8,9,11,12]),
-  new Scale('octatonic (H-W)', diatonic, [0,1,3,4,6,7,9,10,12]),
-  new Scale('octatonic (W-H)', diatonic, [0,2,3,5,6,8,9,11,12]),
-  new Scale('oriental (a)', diatonic, [0,1,4,5,6,8,10,12]),
-  new Scale('oriental (b)', diatonic, [0,1,4,5,6,9,10,12]),
+
+  // new Scale('octatonic (H-W)', diatonic, [0,1,3,4,6,7,9,10,12]),
+  // octatonic (half/whole) AKA diminished half/whole AKA diminished half
+
+  // new Scale('octatonic (W-H)', diatonic, [0,2,3,5,6,8,9,11,12]),
+  // octatonic (whole/half) AKA diminished whole/half
+
+  // new Scale('oriental (a)', diatonic, [0,1,4,5,6,8,10,12]),
+  // oriental (a) scale not found
+
+  new Scale('oriental', diatonic, [0,1,4,5,6,9,10,12]),
+  // oriental AKA maybe oriental (b)
+
   new Scale('overtone', diatonic, [0,2,4,6,7,9,10,12]),
   new Scale('pelog', diatonic, [0,1,3,7,8,12]),
   new Scale('pelog 2', diatonic, [0,1,3,7,10,12]),
@@ -146,18 +211,28 @@ module.exports = [
   new Scale('pentatonic neutral', diatonic, [0,2,5,7,10,12]),
   new Scale('persian', diatonic, [0,1,4,5,6,8,11,12]),
   new Scale('phrygian', diatonic, [0,1,3,5,7,8,10,12]),
-  new Scale('phrygian major', diatonic, [0,1,4,5,7,8,10,12]),
+
+  // new Scale('phrygian major', diatonic, [0,1,4,5,7,8,10,12]),
+  // phrygian major AKA phrygian dominant AKA jewish (ahava rabba) AKA Freygish AKA altered Phrygian AKA dominant b2 b6 AKA Spanish Phrygian AKA Spanish Gypsy AKA Hijaz-Nahawand AKA Bayati maqam AKA Dastgāh-e Homāyoun
+
   new Scale('prometheus', diatonic, [0,2,4,6,9,10,12]),
   new Scale('prometheus neopolitan', diatonic, [0,1,4,6,9,10,12]),
-  new Scale('romanian minor', diatonic, [0,2,3,6,7,9,10,12]),
-  new Scale('six tone symmetrical', diatonic, [0,1,4,5,8,9,12]),
-  new Scale('spanish gypsy', diatonic, [0,1,4,5,7,8,10,12]),
-  new Scale('super locrian', diatonic, [0,1,3,4,6,8,10,12]),
 
-  new Scale('ukranian dorian', diatonic, [0,2,3,6,7,9,10,12]),
+  new Scale('romanian minor', diatonic, [0,2,3,6,7,9,10,12]),
   // ukranian dorian AKA ukranian minor AKA dorian #4 AKA Romanian minor AKA altered Dorian
 
-  new Scale('ultralocrian', diatonic, [0,1,3,4,6,8,9,12]),
+  new Scale('6 tone symmetrical', diatonic, [0,1,4,5,8,9,12]),
+
+  // new Scale('spanish gypsy', diatonic, [0,1,4,5,7,8,10,12]),
+  // Spanish Gypsy AKA phrygian major AKA phrygian dominant AKA jewish (ahava rabba) AKA Freygish AKA altered Phrygian AKA dominant b2 b6 AKA Spanish Phrygian AKA Hijaz-Nahawand AKA Bayati maqam AKA Dastgāh-e Homāyoun
+
+  // new Scale('super locrian', diatonic, [0,1,3,4,6,8,10,12]),
+  // super Locrian AKA altered scale AKA Locrian b4
+
+  // new Scale('ukranian dorian', diatonic, [0,2,3,6,7,9,10,12]),
+  // ukranian dorian AKA ukranian minor AKA dorian #4 AKA Romanian minor AKA altered Dorian
+
+  new Scale('ultra locrian', diatonic, [0,1,3,4,6,8,9,12]),
 
   new Scale('whole tone', diatonic, [0,2,4,6,8,10,12])
   // auxiliary augment AKA Lydian auxiliary augmented AKA whole tone
