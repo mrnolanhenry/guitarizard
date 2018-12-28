c37b323388ee0b34751347782c38c6e54fc04e6c
Jay Querie <jay@querie.cc>
2018-12-28 15:35:28 -0800

    remove .next directory

--- packages/web-ui/.next/static/runtime/webpack.js
+++ packages/web-ui/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5304ce31b278e92e866";
+/******/ 	var hotCurrentHash = "8981775354e331847970";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
