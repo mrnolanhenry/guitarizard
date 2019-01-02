5d2da62889f1753418b31b057e134278de5cb9a2
Jay Querie <jay@querie.cc>
2019-01-02 14:11:30 -0800

    fixup Key tests
    The Notes in the diatonic scale have more information on them (such as
    isFlat, isSharp, and any note aliases in the scale). By using them in
    the tests (vs. creating new Notes), we can ensure that the Notes have
    been set up in the same way.

--- packages/note-lib/test/Key.test.js
+++ packages/note-lib/test/Key.test.js
@@ -4,61 +4,79 @@ const Scale = require('../src/Scale');
 const diatonic = require('../src/data/scaleSystem/diatonic');
 const Key = require('../src/Key');
 
-
-
 // WIP - will finish test file once Key.js is finalized
 tap.test('class Key', function (t) {
-    const BbLydian = new Key(new Note('Bb'), new Scale('lydian', diatonic, [0, 2, 4, 6, 7, 9, 11, 12]));
-    const AsLydian = new Key(new Note('A#'), new Scale('lydian', diatonic, [0, 2, 4, 6, 7, 9, 11, 12]));
 
-    const ANeapolitanMinor = new Key(new Note('A'), new Scale('neapolitan minor', diatonic, [0, 1, 3, 5, 7, 8, 10, 12]));
-    const CMixolydian = new Key(new Note('C'), new Scale('mixolydian', diatonic, [0, 2, 4, 5, 7, 9, 10, 12]));
-    const DAeolian = new Key(new Note('D'), new Scale('aeolian', diatonic, [0, 2, 3, 5, 7, 8, 10, 12]));
-    const ELocrian = new Key(new Note('E'), new Scale('locrian', diatonic, [0, 1, 3, 5, 6, 8, 10, 12]));
-    const FIonian = new Key(new Note('F'), new Scale('ionian', diatonic, [0, 2, 4, 5, 7, 9, 11, 12]));
-    const FMajor = new Key(new Note('F'), new Scale('major', diatonic, [0, 2, 4, 5, 7, 9, 11, 12]));
-    const GDorian = new Key(new Note('G'), new Scale('dorian', diatonic, [0, 2, 3, 5, 7, 9, 10, 12]));
+  const lydianScale = new Scale(
+    'lydian', diatonic,
+    [0, 2, 4, 6, 7, 9, 11, 12]);
+  const neapolitanMinorScale = new Scale(
+    'neapolitan minor', diatonic,
+    [0, 1, 3, 5, 7, 8, 10, 12]);
+  const mixolydianScale = new Scale(
+    'mixolydian', diatonic,
+    [0, 2, 4, 5, 7, 9, 10, 12]);
+  const aeolianScale = new Scale(
+    'aeolian', diatonic,
+    [0, 2, 3, 5, 7, 8, 10, 12]);
+  const locrianScale = new Scale(
+    'locrian', diatonic,
+    [0, 1, 3, 5, 6, 8, 10, 12]);
+  const ionianScale = new Scale(
+    'ionian', diatonic,
+    [0, 2, 4, 5, 7, 9, 11, 12]);
+  const majorScale = new Scale(
+    'major', diatonic,
+    [0, 2, 4, 5, 7, 9, 11, 12]);
+  const dorianScale = new Scale(
+    'dorian', diatonic,
+    [0, 2, 3, 5, 7, 9, 10, 12]);
 
-    const BbLydianEquivKeys = BbLydian.getEquivKeys();
-    // console.log(BbLydianEquivKeys[0])
-    // console.log('compare')
-    // console.log([new Key(new Note('A'), new Scale('neapolitan minor', diatonic, [0, 1, 3, 5, 7, 8, 10, 12]))])
+  const BbLydian         = new Key(diatonic.Bb, lydianScale);
+  const AsLydian         = new Key(diatonic.As, lydianScale);
+  const ANeapolitanMinor = new Key(diatonic.A, neapolitanMinorScale);
+  const CMixolydian      = new Key(diatonic.C, mixolydianScale);
+  const DAeolian         = new Key(diatonic.D, aeolianScale);
+  const ELocrian         = new Key(diatonic.E, locrianScale);
+  const FIonian          = new Key(diatonic.F, ionianScale);
+  const FMajor           = new Key(diatonic.F, majorScale);
+  const GDorian          = new Key(diatonic.G, dorianScale);
 
-    t.same(BbLydian.scale, new Scale('lydian', diatonic, [0, 2, 4, 6, 7, 9, 11, 12]), 'scale identified');
+  const BbLydianEquivKeys = BbLydian.getEquivKeys();
+  const AsLydianEquivKeys = AsLydian.getEquivKeys();
 
-    t.same(BbLydian.note, new Note('Bb'), 'flat note identified');
-    t.same(AsLydian.note, new Note('A#'), 'sharp note identified');
+  t.same(BbLydian.scale, lydianScale, 'scale identified');
 
+  t.same(BbLydian.note, diatonic.Bb, 'flat note identified');
+  t.same(AsLydian.note, diatonic.As, 'sharp note identified');
 
-    // Below 4 tests not working 
-    t.same(BbLydianEquivKeys[0], ANeapolitanMinor, 'equivalent key 0 identified given flat note');
-    t.same(AsLydianEquivKeys[0], ANeapolitanMinor, 'equivalent key 0 identified given sharp note');
+  t.same(BbLydianEquivKeys[0], ANeapolitanMinor, 'equivalent key 0 identified given flat note');
+  t.same(AsLydianEquivKeys[0], ANeapolitanMinor, 'equivalent key 0 identified given sharp note');
 
-    t.same(BbLydian.getEquivKeys(), [
-        ANeapolitanMinor,
-        BbLydian,
-        CMixolydian,
-        DAeolian,
-        ELocrian,
-        FIonian,
-        FMajor,
-        GDorian
-    ], 'equivalent keys identified given flat note');
+  t.same(BbLydian.getEquivKeys(), [
+    ANeapolitanMinor,
+    BbLydian,
+    CMixolydian,
+    DAeolian,
+    ELocrian,
+    FIonian,
+    FMajor,
+    GDorian
+  ], 'equivalent keys identified given flat note');
 
-    t.same(AsLydian.getEquivKeys(), [
-        ANeapolitanMinor,
-        BbLydian,
-        CMixolydian,
-        DAeolian,
-        ELocrian,
-        FIonian,
-        FMajor,
-        GDorian
-    ], 'equivalent keys identified given sharp note');
-    // Above 4 tests not working
+  t.same(AsLydian.getEquivKeys(), [
+    ANeapolitanMinor,
+    BbLydian,
+    CMixolydian,
+    DAeolian,
+    ELocrian,
+    FIonian,
+    FMajor,
+    GDorian
+  ], 'equivalent keys identified given sharp note');
 
-    t.equal(BbLydian.valueOf(), JSON.stringify(BbLydian), 'valueOf works');
-    t.equal(BbLydian.toString(), JSON.stringify(BbLydian), 'toString works');
+  t.equal(BbLydian.valueOf(), JSON.stringify(BbLydian), 'valueOf works');
+  t.equal(BbLydian.toString(), JSON.stringify(BbLydian), 'toString works');
 
-    t.end();
+  t.end();
 });
