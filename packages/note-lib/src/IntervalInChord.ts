152a355fdea4602cfd8ba0539d9cc84df078f491
mrnolanhenry <mr.nolanhenry@gmail.com>
2025-05-09 18:51:33 -0400

    chordTypes WIP stopped at aug7add9

--- packages/note-lib/src/IntervalInChord.ts
+++ packages/note-lib/src/IntervalInChord.ts
@@ -1,11 +1,14 @@
 import { IIntervalAlias } from "./interfaces/IIntervalAlias";
 import { Interval } from "./Interval";
 
+// NOLAN TODO:
+// Consider just adding an optional priority property to the Interval class itself, and removing this class entirely.
+// This would make the code cleaner, and would allow for a more consistent interface for all Intervals, regardless of their use case.
+
 // IntervalInChord used to represent both an Interval, and its priority/importance within a chord
 // This way many more chords can be represented on instruments with fewer strings
 // e.g. the maj13#11 chord cannot be played on a six-string guitar, but it could be approximated using most of the notes.
 // Priority defaults to 1, but can be set to higher numbers for intervals that are less important to the particular chord
-
 export class IntervalInChord extends Interval {
   priority: number;
 
